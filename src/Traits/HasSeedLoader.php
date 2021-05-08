3c5b9521b4fbb513df59d300a2bc6c2ce5e10f95
Julian Tanjung <gudangsoft@gmail.com>
2021-05-08 18:53:07 +0800

    seed loader traits

---
+++ src/Traits/HasSeedLoader.php
@@ -0,0 +1,31 @@
+<?php namespace Pehape\Traits;
+
+trait HasSeedLoader
+{
+
+    /**
+     * Seed provider endpoint
+     * @var string
+     */
+    protected $endpoint;
+
+    /**
+     * Start seeding
+     *
+     * @return self
+     */
+    public function Seed()
+    {
+        $this->Load($this->endpoint)->doseed();
+        return $this;
+    }
+
+    /**
+     * An abstract method to extract the seed data.
+     * This method must be re-define in the child class.
+     *
+     * @return self
+     */
+    abstract protected function doseed();
+
+}
